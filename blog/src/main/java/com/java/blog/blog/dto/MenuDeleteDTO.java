5545199b6a143fbd84f36684ab94185364d7893f
kang <swparabellum@gmail.com>
2025-03-24 12:49:20 +0900

    기타: 파일교통정리 하면서 미반영된 코드 반영

---
+++ blog/src/main/java/com/java/blog/blog/dto/MenuDeleteDTO.java
@@ -0,0 +1,15 @@
+package com.java.blog.blog.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class MenuDeleteDTO {
+
+    private int no;
+
+    private String name;
+}
